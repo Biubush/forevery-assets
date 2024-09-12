a134639e98153c5c3e83f71e5b7628852348adc8
Biubush <1577141450@qq.com>
2024-09-12 10:17:38 +0800

    1

--- js/pages/index.js
+++ js/pages/index.js
@@ -30,8 +30,3 @@ document.querySelectorAll('.note-card').forEach((element) => {
         element.style.backgroundColor = 'rgba(1, 1, 1, 1.5)';
     }
 });
-// 当页面所有资源都加载完成后，隐藏 loading-overlay
-window.onload = function() {
-    document.querySelector('.loading-overlay').style.display = 'none';
-    console.log('页面加载完成！');
-};
